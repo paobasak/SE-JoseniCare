61ef1d82e1e1d86c3289ec82e02bbb8efa6adaf4
Jure B. <jurerhoannebatohanon9597@gmail.com>
2025-12-07 02:47:36 -0800

    add invetory database

---
+++ BACKEND/database/inventory.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE inventory_database;
+USE inventory_database;
+DROP TABLE items;
+
+CREATE TABLE items (
+	itemId VARCHAR (4) PRIMARY KEY,
+    itemName VARCHAR (100) NOT NULL,
+    category VARCHAR (100) NOT NULL,
+    quantity INT NOT NULL,
+    unit VARCHAR (15) NOT NULL,
+    description VARCHAR (100),
+    status VARCHAR (15),
+	expirationDate DateTime NOT NULL
+);
+
+CREATE TABLE itemAlert (
+	alertId VARCHAR (10) PRIMARY KEY,
+    itemId VARCHAR (40), FOREIGN KEY (itemId) REFERENCES items(itemId) ON DELETE CASCADE,
+    type VARCHAR (100),
+    generatedAt DateTime
+);
+
+SELECT * FROM items;
+SELECT * FROM itemAlert;
